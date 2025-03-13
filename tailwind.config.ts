e147c413bde12356e6c31f80721c7973365ea6b8
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-03-13 14:11:02 +0000

    DictionariesAndConfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        orange: '#F58700',
+        'dark-blue': '#070a13',
       },
     },
   },
