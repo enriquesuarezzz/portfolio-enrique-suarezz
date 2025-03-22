62f175dd3df9bf63f302cb279db9727a469748fb
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-03-22 21:03:55 +0000

    pageUpdate

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,21 @@
+import { Experience } from '@/components/molecules/experience/experience'
 import { Header } from '@/components/molecules/header/header'
 import { getTranslations } from 'next-intl/server'
 
 export async function generateMetadata() {
-  const t = await getTranslations('home_page')
+  const t = await getTranslations('metadata')
 
   return {
-    title: t('meta_title'),
-    description: t('meta_description'),
+    title: t('title'),
+    description: t('description'),
   }
 }
 
 export default async function HomePage() {
-  const t = await getTranslations('navbar')
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
+      <Experience />
     </main>
   )
 }
